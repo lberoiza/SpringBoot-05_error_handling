ab784c087af90b240a4188d72c14aa65cb570180
Luis B <luis.beroiza@gmail.com>
2023-05-26 08:52:32 +0200

    Iniciando Proyecto SpringBoot para manejo de errores

---
+++ src/main/java/com/springboot/error_handling/app/SpringBootErrorHandlingApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.error_handling.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootErrorHandlingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootErrorHandlingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springboot/error_handling/app/SpringBootErrorHandlingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.error_handling.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootErrorHandlingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
